734a15eb9deb4e827081694d919c95e61969e825
Kaloyan Velichkov <kaloyan@Kaloyans-MacBook-Pro.local>
2023-01-22 22:08:26 +0200

    quiz component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Quiz from "./components/Quiz";
+import Button from "./components/Quiz/Button";
 
 function App() {
   return <>
     <h1>Quiz App</h1>
+    <Quiz vapros="Колко бързо бягат тигрите?">
+      <Button isCorrect={false}>20 км/ч</Button>
+      <Button isCorrect={true}>64 км/ч</Button>
+      <Button isCorrect={false}>80 км/ч</Button>
+    </Quiz>
+
+    <Quiz vapros="Колко са дълги опашките на тигрите?">
+      <Button isCorrect={false}>30 см</Button>
+      <Button isCorrect={false}>80 см</Button>
+      <Button isCorrect={true}>120 см</Button>
+    </Quiz>
   </>;
 }
 
---
+++ src/components/Quiz/Button.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export default function Button(props) {
+    return <>
+        <div className={props.isCorrect ? "correct" : "incorrect"}>
+            <p>{props.children}</p>
+        </div>
+    </>;
+}
