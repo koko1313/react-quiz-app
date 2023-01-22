bdecb5cbb3abc54df6ab4d8a3f7b0eb73007731c
koko1313 <kaloyanvellichkov@gmail.com>
2023-01-22 22:11:37 +0200

    quiz component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ function App() {
       <Button isCorrect={false}>80 см</Button>
       <Button isCorrect={true}>120 см</Button>
     </Quiz>
+
+    <Quiz vapros="Колко са дълги опашките на тигрите?">
+      <Button isCorrect={false}>30 см</Button>
+      <Button isCorrect={false}>80 см</Button>
+      <Button isCorrect={true}>120 см</Button>
+    </Quiz>
   </>;
 }
 
