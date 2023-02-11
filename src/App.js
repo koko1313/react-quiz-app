69aa470c181df830b98fd74a8a87f24efb289185
Kaloyan Velichkov <kaloyanvellichkov@gmail.com>
2023-02-11 21:18:46 +0200

    fix padding bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Button from "./components/Quiz/Button";
 
 function App() {
   return <>
-    <h1>Quiz App</h1>
+    <h1 className="heder-h1">Quiz App</h1>
     <Quiz vapros="Колко бързо бягат тигрите?">
       <Button isCorrect={false}>20 км/ч</Button>
       <Button isCorrect={true}>64 км/ч</Button>
